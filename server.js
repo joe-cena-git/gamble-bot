cbef391d12c1efa17419bef4536bf2bed19952cd
Joseph Cenatiempo <joecenatiempo@gmail.com>
2015-11-02 20:43:22 +0000

    Dice rolling functionality complete

--- server.js
+++ server.js
@@ -21,8 +21,6 @@ slack.on('message', function(msg){
   
   if(msg.text !== undefined){
     
-    console.log("Received a message.");
-    
     //the following block of logic gets a list of user ids mentioned in the message  
     if(msg.text.indexOf("<@") > -1){
       var messageSegments = msg.text.split("<@");
@@ -35,7 +33,15 @@ slack.on('message', function(msg){
 
     //if the first user mentioned was this bot, we take action
     if(usersMentioned[1] === slack.self.id){
+      
+      //if the text contains "roll", "die", or "dice", we'll send a random dice result back
+      if(msg.text.toLowerCase().indexOf("roll") > -1 || msg.text.toLowerCase().indexOf("dice") > -1 || msg.text.toLowerCase().indexOf("die") > -1){
+        console.log("<@" + sender.id + "> rolled the dice!");
+        channel.send(rollDice(2));
+      } 
+      else //we only want to handle one game at a time
       if(msg.text.toLowerCase().indexOf("flip") > -1 || msg.text.toLowerCase().indexOf("coin") > -1){
+        // if "coin" or "flip" were mentioned, we're starting a coin flip game
         if(usersMentioned[2] !== undefined){
           channel.send("<@" + sender.id + "> is flipping a coin, <@" + usersMentioned[2] + ">: call heads or tails!");
           console.log("<@" + sender.id + "> challenged <@" + usersMentioned[2] + ">.");
@@ -90,6 +96,34 @@ slack.login();
 
 function flippedCoinWasHeads(){
   return (Math.floor(Math.random() * 2) == 0);
+}
+
+function rollDice(howMany){
+  var resultString = "";
+  for(var i=0; i<howMany; i++){
+    var rand = Math.floor(Math.random() * 6) + 1;
+    switch(rand){
+      case 1:
+        resultString += ":one:";
+        break;
+      case 2:
+        resultString += ":two:";
+        break;
+      case 3:
+        resultString += ":three:";
+        break;
+      case 4:
+        resultString += ":four:";
+        break;
+      case 5:
+        resultString += ":five:";
+        break;
+      case 6:
+        resultString += ":six:";
+        break;
+    }
+  }
+  return resultString;
 }
 
 
